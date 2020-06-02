1c905a4a27c9a7b7996d79d84903a14f6a879827
Alessa <alessa_hardwick@live.co.uk>
2020-06-02 14:07:58 +0000

    added fizzbuzz rspec and program

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,6 +1,12 @@
 require_relative '../lib/fizzbuzz'
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
+  it 'putses numbers 1-100 apart from : returns "fizz" when n divisible by 3, returns "buzz" when divisible by 5,
+  returns "fizzbuzz" when divisible by 5 and 3 ' do
     expect(fizzbuzz(3)).to eq 'fizz'
+    expect(fizzbuzz(5)).to eq 'buzz'
+    expect(fizzbuzz(6)).to eq 'fizz'
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+    expect(fizzbuzz(30)).to eq 'fizzbuzz'
+    expect(fizzbuzz(47)).to eq 47
   end
 end
